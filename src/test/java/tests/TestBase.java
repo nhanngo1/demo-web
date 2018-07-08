1e74b2ef6432e1f9a662fc12e83c1272a8fdeea4
Nhan Ngo <nhan.ngo@news.com.au>
2018-07-08 17:19:23 +0700

    add .env

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -4,27 +4,29 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import io.github.cdimascio.dotenv.Dotenv;
 
 import java.net.URL;
 import java.util.Date;
-import java.util.ResourceBundle;
 
 public class TestBase {
     public WebDriver driver;
-    public static ResourceBundle rb;
 
     public void launchBrowser() {
 
-        // get config
-        rb = ResourceBundle.getBundle("config");
-        String RUN_VIA_REMOTE_DRIVER = rb.getString("RUN_VIA_REMOTE_DRIVER");
+        Dotenv dotenv = Dotenv.configure().directory("./").load();
+        String RUN_VIA_REMOTE_DRIVER = dotenv.get("RUN_VIA_REMOTE_DRIVER");
 
         if(RUN_VIA_REMOTE_DRIVER.toUpperCase().equals("YES") == false) {
             driver = new ChromeDriver();
         } else {
             try {
                 DesiredCapabilities chromeCapabilities = DesiredCapabilities.chrome();
-                driver = new RemoteWebDriver(new URL("http://0.0.0.0:4444/wd/hub"), chromeCapabilities);
+
+                String HUB_HOST = dotenv.get("HUB_HOST");
+                String HUB_PORT = dotenv.get("HUB_PORT");
+
+                driver = new RemoteWebDriver(new URL("http://" + HUB_HOST + ":" + HUB_PORT + "/wd/hub"), chromeCapabilities);
             } catch (Exception ex) {
             }
         }
